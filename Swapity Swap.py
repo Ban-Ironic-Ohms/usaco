098c12c99ec858ed4d36fc9d46048346875abd55
Micah Robinson <micahrobinson93@gmail.com>
2020-07-21 11:12:53 -0700

    working mroe!

--- Swapity Swap.py
+++ Swapity Swap.py
@@ -6,15 +6,43 @@ metadata = metadata.split()
 n = metadata[0]
 K = metadata[1]
 K = int(K)
+n = int(n)
 
+swap1 = file_in.readline()
+swap2 = file_in.readline()
 
-#getting all swaps
-swaps = []
+cows = []
+i = 1
 
-swap_num = 1
+while i <= n:
+    cows.append(i)
+    i += 1
 
-while swap_num < K:
-    swaps.append((file_in.readline())
-    swap_num += 1
+def swap(one, two, list):
+    try:
+        one = int(one)
+        one -= 1
+        two = int(two)
+        two -= 1
+    except ValueError:
+        print("Oh no! not a valid input")
+        exit
 
-print(swaps)
+    first = list[one]
+    second = list[two]
+
+    list[one] = second
+    list[two] = first
+
+    print(cows)
+
+i = 1
+while i <= K:
+    a = swap1.split()[0] #2
+    b = swap1.split()[1] #5
+    c = swap2.split()[0] #3
+    d = swap2.split()[1] #7
+    swap(a, b, cows)
+    swap(c, d, cows)
+    i += 1
+
