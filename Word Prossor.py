c684a6961a8715196dc6ad3b2758dd4f7795ac75
Micah Robinson <micahrobinson93@gmail.com>
2020-07-24 12:13:23 -0700

    one last bug...

--- Word Prossor.py
+++ Word Prossor.py
@@ -1,10 +1,11 @@
 #opening file
-file_in = open(r"C:\Users\Micah\Desktop\Personal\Python\usaco\Word Processor\1.in")
+file_in = open(r"C:\Users\Micah\Desktop\Personal\Python\usaco\Word Processor\2.in")
 
 #getting vars
 metadata = file_in.readline()
 total_words = metadata.split()[0]
 chars_per_line = metadata.split()[1]
+chars_per_line = int(chars_per_line)
 
 essay = file_in.readline()
 
@@ -13,11 +14,26 @@ words = essay.split()
 
 #making the lines
 current_chars = 0
-line1 = ""
-currentline = line1
-for item in words:
-    if len(item) < (chars_per_line - current_chars):
-        currentline = currentline + " " + item
-    elif len(item) > (chars_per_line - current_chars):
-        currentline = 
+lines = []
+currentline = ""
 
+for item in words:
+
+    if len(item) <= (chars_per_line - current_chars):
+        currentline = currentline + item + " "
+        current_chars += len(item)
+
+    elif len(item) >= (chars_per_line - current_chars):
+        lines.append(currentline)
+ 
+        currentline = ""
+        current_chars= 0
+        currentline = currentline + item + " "
+        current_chars += len(item)
+
+    if item == words[-1]:
+        lines.append(item + " ")
+
+unspaced = [sub[:-1] for sub in lines]
+
+print(unspaced)
