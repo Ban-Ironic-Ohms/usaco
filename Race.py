91a116e7822bffaa1c5fcbaacaef23367e1accc8
Micah Robinson <micahrobinson93@gmail.com>
2020-07-25 14:14:09 -0700

    anothe commit!

--- Race.py
+++ Race.py
@@ -4,6 +4,7 @@ file_in = open(r"C:\Users\Micah\Desktop\Personal\Python\usaco\Race\1.in")
 #getting data
 metadata = file_in.readline()
 race_length = metadata.split()[0]
+race_length = int(race_length)
 speeds = metadata.split()[1]
 speeds = int(speeds)
 
@@ -14,15 +15,12 @@ while i < speeds:
     all_speeds.append(file_in.readline()[:-1])
     i += 1
 
-#setting params
-distance_traveled = 0
-speed = 0
 
 #function to see if you can slow down in time
 def slow_down(speed, max_speed, distance_traveled, race_length):
     done = False
     while done == False:
-        if speed <= max_speed:
+        if speed <= int(max_speed):
             return True
         speed = speed - 1
         distance_traveled = distance_traveled + speed
@@ -33,4 +31,19 @@ def slow_down(speed, max_speed, distance_traveled, race_length):
     else:
         return False
 
-print(slow_down(10, 3, 0, 8))
+#calculate minimumn time
+def min_time(max_speed, race_length):
+    distance_traveled = 0
+    speed = 0
+    done = False
+    time = 0
+    while done == False:
+        if slow_down(speed, max_speed, distance_traveled, race_length) == True:
+            speed += 1
+            distance_traveled += speed
+            time += 1
+        if race_length - distance_traveled <= 0:
+            done = True
+    return(time)
+
+print(min_time(all_speeds[0], race_length))
